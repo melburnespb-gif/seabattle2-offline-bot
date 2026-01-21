5749ee3e96d1ee151a328a17cb015ce1af323db6
melburnespb-gif <melburne.spb@gmail.com>
2026-01-22 01:55:36 +0300

    Update MainActivity.kt

--- app/src/main/java/com/seabattle2/offlinebot/MainActivity.kt
+++ app/src/main/java/com/seabattle2/offlinebot/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.seabattle2.offlinebot
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val tv = TextView(this)
-        tv.text = "SeaBattle2 Offline Bot\nAPK собран успешно"
-        tv.textSize = 18f
-        setContentView(tv)
+        if (!Settings.canDrawOverlays(this)) {
+            Toast.makeText(this, "Дай разрешение 'Поверх других окон'", Toast.LENGTH_LONG).show()
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivity(intent)
+        } else {
+            startService(Intent(this, OverlayService::class.java))
+            Toast.makeText(this, "Overlay запущен (кнопка BOT)", Toast.LENGTH_LONG).show()
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Settings.canDrawOverlays(this)) {
+            startService(Intent(this, OverlayService::class.java))
+            finish()
+        }
     }
 }
